0af4e3e4d7da361198618c7988d62b1d93c8c87a
Gyan <gyanprakashdangi@gmail.com>
2024-07-08 20:54:55 +0530

    Tour Plan using react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+// import data from "./data"
+import { useState } from "react";
+import Tours from "./Components/Tours";
+import data from './data'
+
+const App = () => {
+
+  const [tours, setTours]= useState(data);
+
+  function removeTour(id){
+      const newTours =tours.filter(tour=> tour.id !==id);
+
+      setTours(newTours);
+  }   
+
+  if(tours.length ===0){
+    return(
+      
+      <div className="refresh">
+          <h2>No Tours Left</h2>
+          <button onClick={()=> setTours(data)} className="btn-white">Refresh</button>
+      </div>
+
+    )
+  }
+
+  return (
+    <div  className="App">
+
+
+      <Tours tours ={tours} removeTour={removeTour}></Tours>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/CardDetail.js
@@ -0,0 +1,15 @@
+import Card from "./Card";
+function CardDetail(id){
+	
+
+	return(
+
+		<div>
+			<Card id ={id}>
+
+			</Card>
+		</div>
+	)
+}
+
+export default CardDetail;
